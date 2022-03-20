c876524b89d94e84d9ce391f240b970db4badad2
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-03-20 08:38:30 -0400

    finished files

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,37 @@
 import React from "react";
-import Data from "../Data";
-import mountFuji from "../images/chris-fowler-78XvFtNTCBw-unsplash.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
-export default function Card() {
+export default function Card(props) {
+  function capitalize(str) {
+    return str.toUpperCase();
+  }
   return (
     <div className="card">
-      <img src={mountFuji} className="card--img" />
+      <img src={props.card.imageUrl} className="card--img" />
       <div className="card--info">
         <div className="card--location">
           <FontAwesomeIcon
             icon={faMapMarkerAlt}
             className="card--location-icon"
           />
-          <div className="card--location-name">JAPAN</div>
-          <a className="card--location-google">View on Google Maps</a>
+          <div className="card--location-name">
+            {capitalize(props.card.location)}
+          </div>
+          <a
+            className="card--location-google"
+            href={props.card.googleMapsUrl}
+            target="_blank"
+          >
+            View on Google Maps
+          </a>
         </div>
-        <h2 className="card--title">Mount Fuji</h2>
+        <h2 className="card--title">{props.card.title}</h2>
         <div className="card--date">
-          <span className="card--start-date">12 Jan, 2021</span>-
-          <span className="card--end-date">24 Jan, 2021</span>
+          <span className="card--start-date">{props.card.startDate}</span>-
+          <span className="card--end-date">{props.card.endDate}</span>
         </div>
-        <p className="card--description">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <p className="card--description">{props.card.description}</p>
       </div>
     </div>
   );
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Card from "./Card";
+import data from "../Data";
 
 export default function Content() {
-  return (
-    <section className="content">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </section>
-  );
+  const cards = data.map((card) => {
+    return <Card card={card} />;
+  });
+  return <section className="content">{cards}</section>;
 }
